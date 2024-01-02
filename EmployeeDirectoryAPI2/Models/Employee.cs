ee366f2a603db6b28a43cbd1a00e32fb4bdf60fe
abhiram1117 <abhiram1117@gmail.com>
2024-01-02 12:53:52 +0530

    DB first approach

---
+++ EmployeeDirectoryAPI2/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmployeeDirectoryAPI2.Models
+{
+    public partial class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string JobTitle { get; set; } = null!;
+        public string Department { get; set; } = null!;
+        public string? Location { get; set; }
+    }
+}
---
+++ EmployeeDirectoryAPI2/Models/EmployeeDirectoryAPIDataContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EmployeeDirectoryAPI2.Models
+{
+    public partial class EmployeeDirectoryAPIDataContext : DbContext
+    {
+        public EmployeeDirectoryAPIDataContext()
+        {
+        }
+
+        public EmployeeDirectoryAPIDataContext(DbContextOptions<EmployeeDirectoryAPIDataContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employees { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=EmployeeDirectoryAPI.Data;Trusted_Connection=true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
