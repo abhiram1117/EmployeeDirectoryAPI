181ce3b93c24acec23ce65240b2ce8225b8bb650
abhiram1117 <abhiram1117@gmail.com>
2024-01-03 00:26:12 +0530

    API response model

---
+++ EmployeeDirectoryAPI2/Models/ApiResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace EmployeeDirectoryAPI2.Models
+{
+    public class ApiResponse<T>
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+    }
+}
